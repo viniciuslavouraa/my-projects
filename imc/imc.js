82f55b97c8da7f5c1d73d908f56a03fc867f411c
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-02-18 15:20:38 -0300

    finalizado calculador de IMC

--- imc/imc.js
+++ imc/imc.js
@@ -7,6 +7,7 @@ btn_m.addEventListener ('click',homem)
 btn_f.addEventListener ('click', mulher)
 img.setAttribute ('id', 'img')
 function homem () {
+    document.body.getElementsByTagName('section')[0].style.height = '470px'
     btn_f.style.display = 'none'
     btn_m.style.display = 'none'
     img.setAttribute ('src','./images/homem-p.png')
@@ -15,10 +16,11 @@ function homem () {
     sexo_img.appendChild(img)
 }
 function mulher () {
+    document.body.getElementsByTagName('section')[0].style.height = '470px'
     btn_f.style.display = 'none'
     btn_m.style.display = 'none'
     var genero = 'mulher'
-    sexo_img.innerHTML = `Uma ${genero} <br>`
+    sexo_img.innerHTML = `<p>Uma ${genero}</p> <br>`
     img.setAttribute ('src', './images/mulher-p.png')
     sexo_img.appendChild (img)
 }
@@ -29,30 +31,31 @@ btn_limpa.addEventListener ('clickL', limpar)
 btn_calc.addEventListener ('click', calcular)
 
 function limpar () {
-
+    
 }
 
 function calcular () {
+    document.body.getElementsByTagName('section')[0].style.height = '586px'
     var clas = document.body.getElementsByTagName ('ul')[0]
     var imc_res = document.querySelector ('#imc-result')
     var idade = document.querySelector ('#idade')
     var altura = document.querySelector ('#altura')
     var peso = document.querySelector ('#peso')
     const imc = peso.value / (altura.value * altura.value)
-    imc_res.innerHTML += `Seu IMC é de ${imc.toFixed(2)}`
+    var result = ''
     if (imc < 18.5) {
-        imc_res.innerHTML += `<p>Abaixo do peso</p>`
+        result = `<p>Abaixo do peso</p>`
     } else if (imc < 24.9) {
-        imc_res.innerHTML += `<p>Peso Normal</p>`
+        result = `<p>Peso Normal</p>`
     } else if (imc >= 25.0 && imc <= 29.9) {
-        imc_res.innerHTML += `<p>Pré-Obesidade</p>`
+        result = `<p>Pré-Obesidade</p>`
     } else if (imc >= 30.0 && imc <= 34.9){
-        imc_res.innerHTML += `<p>Obesidade Grau 1</p>`
+        result+= `<p>Obesidade Grau 1</p>`
     } else if (imc >= 35.0 && imc <= 39.9) {
-        imc_res.innerHTML += `<p>Obesidade Grau 2</p>`
+        result = `<p>Obesidade Grau 2</p>`
     }else {
-        imc_res.innerHTML += `<p>Obesidade Grau 3</p>`
+        result = `<p>Obesidade Grau 3</p>`
     }
-    imc_res.innerHTML += `<div id="dados">Dados informados: Idade:${idade.value}anos Altura:${altura.value}m Peso: ${peso.value}(kg)
+    imc_res.innerHTML += `<div id="dados">Seu IMC é de ${imc.toFixed(2)} ${result} Dados informados: <br> Idade:${idade.value} <br> anos Altura:${altura.value}m <br> Peso: ${peso.value}(kg)
     </div>`
 }
