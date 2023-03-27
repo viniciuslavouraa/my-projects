6caeafeb0f5e9dde911fc8dd3d54944b35f28f65
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-03-27 15:56:54 -0300

    :+1:

--- RickandMorty/js/api.js
+++ RickandMorty/js/api.js
@@ -34,13 +34,20 @@ async function getAllCharacters() {
             characters.secondary[firstName] = {image, id, name, gender, status, location, origin, species}
         }
 
+
     })
     //Create elements
     const personagemBox = document.querySelector('#personagens')
     const divImagens = document.createElement('div')
     const divSoloImage = document.createElement('div')
+    const image = document.createElement('div')
     const imgRick = document.createElement('img')
+    const imgRickAdjudicator = document.createElement('img')
+    const imgRickAlien = document.createElement('img')
+    const imgRickAntenna = document.createElement('img')
     const imgMorty = document.createElement('img')
+    const imgMortyAlien = document.createElement('img')
+    const imgMortyAntenna = document.createElement('img')
     const imgSummer = document.createElement('img')
     const imgBeth = document.createElement('img')
     const imgJerry = document.createElement('img')
@@ -54,10 +61,35 @@ async function getAllCharacters() {
     const imgAmish = document.createElement('img')
     const imgAnnie = document.createElement('img')
     const imgAnts = document.createElement('img')
+    // Create characters information 
+    const informationMorty = document.createElement('p')
+    informationMorty.innerHTML = ``
+    const informationMortyAlien = document.createElement('p')
+    informationMorty.innerHTML = ``
+    const informationMortyAntenna = document.createElement('p')
+    informationMortyAntenna.innerHTML = ``
+    const allInfo = {informationMorty, informationMortyAlien,informationMortyAntenna}
+    Object.values(allInfo).forEach((infos) => {
+        infos.classList.add('text')
+        infos.innerHTML = `
+        Gender: <br>
+        id: <br>
+        Location:<br> <br>
+        Name: <br>
+        Origin: <br
+        Species: <br>
+        Status: <br>
+        `
+    })
     //Add classList to elemnts
     divSoloImage.classList.add('divImagens')
     imgRick.classList.add('img')
+    imgRickAdjudicator.classList.add('img')
+    imgRickAlien.classList.add('img')
+    imgRickAntenna.classList.add('img')
     imgMorty.classList.add('img')
+    imgMortyAlien.classList.add('img')
+    imgMortyAntenna.classList.add('img')
     imgSummer.classList.add('img')
     imgBeth.classList.add('img')
     imgJerry.classList.add('img')
@@ -73,7 +105,13 @@ async function getAllCharacters() {
     imgAnts.classList.add('img')
     // Set source to images
     imgRick.setAttribute('src', characters.ricks.Rick.image)
+    imgRickAdjudicator.setAttribute('src', characters.ricks.Adjudicator.image)
+    imgRickAlien.setAttribute('src', characters.ricks.Alien.image)
+    imgRickAntenna.setAttribute('src', characters.ricks.Antenna.image)
     imgMorty.setAttribute('src', characters.familySmith.mortys.Morty.image)
+    imgMortyAlien.setAttribute('src', characters.familySmith.mortys.Alien.image)
+    imgMortyAntenna.setAttribute('src', characters.familySmith.mortys.Antenna.image)
+    
     imgSummer.setAttribute('src', characters.familySmith.Summer.image)
     imgBeth.setAttribute('src', characters.familySmith.Beth.image)
     imgJerry.setAttribute('src', characters.familySmith.Jerry.image)
@@ -90,8 +128,16 @@ async function getAllCharacters() {
     divSoloImage.appendChild(imgBeth)
     divSoloImage.appendChild(imgJerry)
     divSoloImage.appendChild(imgMorty)
+    divSoloImage.appendChild(informationMorty).style.display = 'none'
+    divSoloImage.appendChild(imgMortyAlien).style.display = 'none'
+    divSoloImage.appendChild(informationMortyAlien).style.display = 'none'
+    divSoloImage.appendChild(imgMortyAntenna).style.display = 'none'
+    divSoloImage.appendChild(informationMortyAntenna).style.display = 'none'
     divSoloImage.appendChild(imgSummer)
     divSoloImage.appendChild(imgRick)
+    divSoloImage.appendChild(imgRickAdjudicator).style.display = 'none'
+    divSoloImage.appendChild(imgRickAlien).style.display = 'none' 
+    divSoloImage.appendChild(imgRickAntenna).style.display = 'none'
     divSoloImage.appendChild(imgAbradolf)
     divSoloImage.appendChild(imgAgency)
     divSoloImage.appendChild(imgAlan)
@@ -111,21 +157,87 @@ async function getAllCharacters() {
     btnVoltar.classList.add('btn-voltar')
     // Continuar a criação de cada imagem com suas informações
     // Array for all images
-    const images = { imgBeth, imgJerry, imgMorty, imgSummer, imgRick, imgAbradolf, imgAgency, imgAlan, imgAlbert, imgAlexander, imgAlien, imgAmish, imgAnnie, imgAnts, imgAbadango}
+        const images = { imgBeth, imgJerry, imgMorty, imgSummer, imgRick, imgAbradolf, imgAgency, imgAlan, imgAlbert, imgAlexander, imgAlien, imgAmish, imgAnnie, imgAnts, imgAbadango}
+        const mortyImages = {imgMorty, imgMortyAlien, imgMortyAntenna}
+        const rickImages = {imgRick, imgRickAdjudicator, imgRickAlien, imgRickAntenna}
 
-    Object.values(images).forEach((imagem) => {
-        imagem.addEventListener('click', () => {
-            // Esconde todas as outras imagens
-            Object.values(images).forEach(outraImagem => {
-            if (outraImagem !== imagem) {
-                outraImagem.style.display = 'none'
-            }
+        Object.values(images).forEach((imagem) => {
+            imagem.addEventListener('click', () => {
+                divSoloImage.classList.add('divImg')
+                Object.values(images).forEach(outraImagem => {
+                    if (outraImagem !== imagem) {
+                        outraImagem.style.display = 'none'
+                    }
+                })
+                if (imagem == imgMorty) {
+                    if (characters.familySmith.mortys.Morty) {
+                        informationMorty.innerHTML = `
+                        Gender:${characters.familySmith.mortys.Morty.gender} <br>
+                        id:${characters.familySmith.mortys.Morty.id} <br>
+                        Location:<br>${characters.familySmith.mortys.Morty.location.name} <br>
+                        Name:${characters.familySmith.mortys.Morty.name} <br>
+                        Origin:${characters.familySmith.mortys.Morty.origin.name} <br
+                        Species:${characters.familySmith.mortys.Morty.species} <br>
+                        Status:${characters.familySmith.mortys.Morty.status} <br>
+                        `
+                        informationMorty.style.display = 'block'
+                    }
+                    if (characters.familySmith.mortys.Alien) {
+                        informationMortyAlien.innerHTML =  `
+                        Gender:  ${characters.familySmith.mortys.Alien.gender} <br>
+                        id:  ${characters.familySmith.mortys.Alien.id} <br>
+                        Location:<br>  ${characters.familySmith.mortys.Alien.location.name} <br>
+                        Name: ${characters.familySmith.mortys.Alien.name} <br>
+                        Origin:  ${characters.familySmith.mortys.Alien.origin.name} <br>
+                        Species:  ${characters.familySmith.mortys.Alien.species} <br>
+                        Status:  ${characters.familySmith.mortys.Alien.status} <br>
+                        `
+                        informationMortyAlien.style.display = 'block'
+                    }
+                    if (characters.familySmith.mortys.Antenna) {
+                        informationMortyAntenna.innerHTML =  `
+                        Gender:${characters.familySmith.mortys.Antenna.gender} <br>
+                        id:${characters.familySmith.mortys.Antenna.id} <br>
+                        Location:<br>${characters.familySmith.mortys.Antenna.location.name} <br>
+                        Name: ${characters.familySmith.mortys.Antenna.name} <br>
+                        Origin:${characters.familySmith.mortys.Antenna.origin.name} <br
+                        Species:${characters.familySmith.mortys.Antenna.species} <br>
+                        Status:${characters.familySmith.mortys.Antenna.status} <br>
+                        `
+                        informationMortyAntenna.style.display = 'block'
+                    }
+                    Object.values(mortyImages).forEach((mortyImage) => {
+                        mortyImage.style.display = 'block'
+                    })
+                } else if (imagem == imgRick) {
+                    Object.values(rickImages).forEach((rickImage) => {
+                        rickImage.style.display = 'block'
+                    })
+                }
+                // Exibe apenas a imagem clicada
+                imagem.style.display = 'block'
             })
-            
-            // Exibe apenas a imagem clicada
-            imagem.style.display = 'block'
         })
-        })
-
 }
 getAllCharacters()
+
+
+/*                // Esconde todas as outras imagens
+                if (imgMorty) {
+                    Object.values(rickImages).forEach((rickImage) => {
+                        rickImage.style.display = 'none'
+                    })
+                    Object.values(mortyImages).forEach((mortyImage) => {
+                        mortyImage.style.display = 'block'
+                    })
+                }
+                if (imgRick) {
+                    Object.values(mortyImages).forEach((mortyImage) => {
+                        mortyImage.style.display = 'none'
+                    })
+                    Object.values(rickImages).forEach((rickImage) => {
+                        rickImage.style.display = 'block'
+                        
+                    })
+                }
+'*/ 
