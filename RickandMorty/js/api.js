4ce0937677ae7c12bc6194bbb6d91d23ada2b6d4
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-04-03 17:53:00 -0300

    :+1:

--- RickandMorty/js/api.js
+++ RickandMorty/js/api.js
@@ -14,6 +14,7 @@ async function getAllCharacters() {
 
         }
     }
+    console.log(data)
     for (let i = 1 ; i <= totalPages; i++) {
         const pageUrl = `https://rickandmortyapi.com/api/character?page=${i}`
         const pageResponse = await fetch(pageUrl)
@@ -21,46 +22,37 @@ async function getAllCharacters() {
         //console.log(pageData)
         pageData.results.forEach((info) => {
             const {image,created, episode, id, name, gender, status, location, origin, species, type} = info
-
+            key = info.name.replace(/\s+/g, '-')
             const [firstName, lastName] = info.name.split(" ")
             
             if(firstName === "Rick" || lastName === "Rick") {
-                characters.ricks[firstName] = {image,created, episode, id, name, gender, status, location, origin, species, type}
+                characters.ricks[firstName] = {image,created, episode, id, name, gender, status, location, origin, species, type, key}
             }
             if (lastName === "Smith" && firstName !== "Morty") {
-                characters.familySmith[firstName] = {image,created, episode, id, name, gender, status, location, origin, species, type}
+                characters.familySmith[firstName] = {image,created, episode, id, name, gender, status, location, origin, species, type, key}
             }
             if (firstName === "Morty" || lastName === "Morty") {
-                characters.familySmith.mortys[firstName] = {image,created, episode, id, name, gender, status, location, origin, species, type}
+                characters.familySmith.mortys[firstName] = {image,created, episode, id, name, gender, status, location, origin, species, type, key}
             }
             if (firstName !== "Rick" && lastName !== "Rick"  && firstName !== "Morty" && lastName !== "Morty"  && lastName !== "Smith") {
-                characters.secondary[firstName] = {image,created, episode, id, name, gender, status, location, origin, species, type}
+                characters.secondary[firstName] = {image,created, episode, id, name, gender, status, location, origin, species, type, key}
             }
         })
     }
-    // adicionar o resto dos personagens
     //Create elements
     const personagemBox = document.querySelector('#personagens')
-    const personagemBox2 = document.querySelector('.perso2')
     const divImagens = document.createElement('div')
     const divSoloImage = document.createElement('div')
-    const divSoloImage2 = document.createElement('div')
     const image = document.createElement('div')
     
     
     const ricks = {}
-    const hiddeRicks = {}
     // Ricks
     Object.values(characters.ricks).forEach((rick) => {
         let img = document.createElement('img')
         img.src = rick.image
         img.setAttribute('key', rick.name.replace(/\s+/g, '-'))
-
-        if(rick.name !== 'Rick Sanchez') {
-            hiddeRicks[rick.name] = img
-        } else  {
-            ricks[rick.name] = img
-        }
+        ricks[rick.name] = img
 
         // Add the first name of rick after the tag img
         const rickName = rick.name.split(' ')[0]
@@ -70,22 +62,15 @@ async function getAllCharacters() {
     })
 
     console.log(ricks)
-    console.log(hiddeRicks)
 
     const mortys = {}
-    const hiddeMortys = {}
     // Mortys
     Object.values(characters.familySmith.mortys).forEach((morty => {
         let img = document.createElement('img')
         img.src = morty.image
         img.setAttribute('key', morty.name.replace(/\s+/g, '-'))
-        if (morty.name !== 'Morty Smith') {
-            hiddeMortys[morty.name] = img
-        } else {
-            mortys[morty.name] = img
-        }
+        mortys[morty.name] = img
         
-
         //Add the firts name of morty after the tag img
         const mortyName = morty.name.split(' ')[0]
         const span = document.createElement('span')
@@ -93,7 +78,6 @@ async function getAllCharacters() {
         img.insertAdjacentElement('afterend',span)
    }))
    console.log(mortys)
-   console.log(hiddeMortys)
 
    const secundaryCharacters = {}
    //Seconde Characters
@@ -110,44 +94,69 @@ async function getAllCharacters() {
         img.insertAdjacentElement('afterend', span)
    })
    console.log(secundaryCharacters)
+
+   // Get all infos
+   let allInfos = {} 
+   Object.values(characters).forEach((character) => {
+        Object.values(character).forEach((charInfo) => {
+            allInfos[charInfo.name] = {
+                created: charInfo.created, gender: charInfo.gender, id: charInfo.id,
+                key: charInfo.key, location:charInfo.location, name: charInfo.name, 
+                origin: charInfo.origin, species: charInfo.species, status: charInfo.status,
+                type: charInfo.type
+            }
+        })
+   })
+   console.log(allInfos)
+
     // Object with all images
     const allImages = {ricks, mortys, secundaryCharacters}
 
     Object.values(allImages).forEach((imgObj) => {
-            Object.values(imgObj).forEach((img) => {
-                img.classList.add('img')
-                divSoloImage.appendChild(img)
-                img.addEventListener('click',(e) => {
-                    img.classList.toggle('flip-card')
-                    const backCard = document.createElement('div')
-                    const imgkey = e.target.getAttribute('key')
-                    const imgParent = e.target.parentNode
-                    // Dar um jeito de adicionar as informações quando o card virar de forma clean
-                    const keys ={} = e.target
-                    console.log(`é o ${imgkey}`)
-                    if (imgkey == 'Rick-Sanchez') {
-                        console.log('é o rick')
-                    }
-                    if (img.classList.contains('flip-card')) {
-                        console.log('teste')
-                        setTimeout(() => {
-                            backCard.classList.toggle('card')
-                            backCard.setAttribute('key',imgkey)
-                            imgParent.replaceChild(backCard, e.target)
-                        }, 400);
-                    }
+        Object.values(imgObj).forEach((img) => {
+            img.classList.add('img')
+            divSoloImage.appendChild(img)
 
-                    backCard.addEventListener('click', () =>{
-                        setTimeout(() => {
-                            imgParent.replaceChild(e.target, backCard)
-                            img.classList.toggle('flip-card')
-                        }, 400);
-                    })
-                    console.log(`Clicked image with key ${e.target.getAttribute('key')}`)
-                })
-                })
+            img.addEventListener('click',(e) => {
+                img.classList.toggle('flip-card')
+                const backCard = document.createElement('div')
+                const imgkey = e.target.getAttribute('key')
+                const imgParent = e.target.parentNode
+                if (img.classList.contains('flip-card')) {
+                    setTimeout(() => {
+                        const p = document.createElement('p')
+                        
+                        backCard.classList.toggle('card')
+                        if (imgkey == 'Rick-Sanchez') {
+                            p.innerHTML = `
+                            ${allInfos['Rick Sanchez'].created}<br>
+                            ${allInfos['Rick Sanchez'].gender} <br>
+                            ${allInfos['Rick Sanchez'].id} <br>
+                            ${allInfos['Rick Sanchez'].location.name} <br>
+                            ${allInfos['Rick Sanchez'].name} <br>
+                            ${allInfos['Rick Sanchez'].origin.name} <br>
+                            ${allInfos['Rick Sanchez'].species} <br>
+                            ${allInfos['Rick Sanchez'].status} <br>
+                            ${allInfos['Rick Sanchez'].type}
+                            `
+                            backCard.appendChild(p)
+                        }
+                        backCard.setAttribute('key',imgkey)
+                        imgParent.replaceChild(backCard, e.target)
+                    }, 400);
+                }
 
-            }) 
+                backCard.addEventListener('click', () =>{
+                    setTimeout(() => {
+                        imgParent.replaceChild(e.target, backCard)
+                        img.classList.toggle('flip-card')
+                    }, 400);
+                })
+                console.log(`Clicked image with key ${e.target.getAttribute('key')}`)
+            })
+        })
+
+    }) 
     //Add classList to elemnts
     divSoloImage.classList.add('divImagens')
     divImagens.appendChild(divSoloImage)
@@ -156,54 +165,18 @@ async function getAllCharacters() {
 }
 getAllCharacters()
 
+
 /*
-                img.addEventListener('click',(e) => {
-                    img.classList.toggle('flip-card')
-
-                    const backCard = document.createElement('div')
-                    const imgkey = e.target.getAttribute('key')
-                    const imgParent = e.target.parentNode
-                    const character = findCharacterByKey(imgkey)
-                    if (character) {
-                        const backCard = document.createElement('div');
-                        backCard.innerHTML = `Created: ${character.created}<br>Gender: ${character.gender}`;
-                        backCard.classList.add('card');
-                        backCard.setAttribute('key', imgkey);
-                        imgParent.replaceChild(backCard, e.target);
-                        img.classList.toggle('flip-card');
-                    }
-                    function findCharacterByKey(key) {
-                        for ( const personagens of Object.values(characters)){
-                            for(const character of Object.values(personagens)) {
-                                if (character.key === key) {
-                                    return character
-                                }
-                            }
-                        }
-                        return null
-                    }
-                    console.log(imgkey)
-                    if (img.classList.contains('flip-card')) {
-                        setTimeout(() => {
-                            backCard.classList.toggle('card')
-                            backCard.setAttribute('key',imgkey)
-                            imgParent.replaceChild(backCard, e.target)
-                        }, 400);
-                    }
-
-                    backCard.addEventListener('click', () =>{
-                        const imgkey = backCard.getAttribute('key');
-                        const character = findCharacterByKey(imgkey);
-                        if (character) {
-                          imgParent.replaceChild(img, backCard);
-                          img.classList.toggle('flip-card');
-                        }
-                        setTimeout(() => {
-                            imgParent.replaceChild(e.target, backCard)
-                            img.classList.toggle('flip-card')
-                        }, 400);
-                    })
-                    console.log(`Clicked image with key ${e.target.getAttribute('key')}`)
-                })
-
-*/
+   // Get all infos
+   let allInfos = {} 
+   Object.values(characters).forEach((character) => {
+        Object.values(character).forEach((charInfo) => {
+            allInfos[charInfo.name] = {
+                created: charInfo.created, gender: charInfo.gender, id: charInfo.id,
+                key: charInfo.key, location:charInfo.location, name: charInfo.name, 
+                origin: charInfo.origin, species: charInfo.species, status: charInfo.status,
+                type: charInfo.type
+            }
+        })
+   })
+    */
